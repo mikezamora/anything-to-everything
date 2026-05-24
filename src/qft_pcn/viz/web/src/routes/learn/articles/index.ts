4ccaf7a98fc5e866a94d327fb10ab9c9cd21514e
mikezamora <contact@mike-zamora.com>
2026-05-23 21:02:56 -0400

    feat(viz/web): register all 19 Learn articles in textbook-outline order

--- src/qft_pcn/viz/web/src/routes/learn/articles/index.ts
+++ src/qft_pcn/viz/web/src/routes/learn/articles/index.ts
@@ -11,6 +11,20 @@ import { article as foundationsVectorsTensors } from './foundations-vectors-tens
 import { article as foundationsHilbertOperators } from './foundations-hilbert-operators';
 import { article as foundationsVariationalFe } from './foundations-variational-fe';
 import { article as foundationsRiemannian } from './foundations-riemannian';
+import { article as qftMps } from './qft-mps';
+import { article as qftMera } from './qft-mera';
+import { article as qftHamiltonian } from './qft-hamiltonian';
+import { article as qftVqc } from './qft-vqc';
+import { article as pcnFields } from './pcn-fields';
+import { article as pcnDynamics } from './pcn-dynamics';
+import { article as pcnMultifield } from './pcn-multifield';
+import { article as fusionManifold } from './fusion-manifold';
+import { article as fusionPcnCoupling } from './fusion-pcn-coupling';
+import { article as fusionQpcn } from './fusion-qpcn';
+import { article as fusionLogic } from './fusion-logic';
+import { article as fusionMeraRelax } from './fusion-mera-relax';
+import { article as fusionBridge } from './fusion-bridge';
+import { article as dslWalkthrough } from './dsl-walkthrough';
 
 // IMPORTANT: when adding a new article, import it here AND push it onto
 // ARTICLES below. The order is the textbook outline.
@@ -21,4 +35,18 @@ export const ARTICLES: ArticleSpec[] = [
   foundationsHilbertOperators,
   foundationsVariationalFe,
   foundationsRiemannian,
+  qftMps,
+  qftMera,
+  qftHamiltonian,
+  qftVqc,
+  pcnFields,
+  pcnDynamics,
+  pcnMultifield,
+  fusionManifold,
+  fusionPcnCoupling,
+  fusionQpcn,
+  fusionLogic,
+  fusionMeraRelax,
+  fusionBridge,
+  dslWalkthrough,
 ];
